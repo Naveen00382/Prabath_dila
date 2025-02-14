0c647f4515c024e6662dc9a7eba53cf9efc46336
Naveen00382 <sanjeewania808@gmail.com>
2025-02-14 20:52:29 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "null"
 const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const SESSION_ID = "PRABATH-MD~vJ0UzJRJ#DC_A--wCp-i7rwD7mhs43AVxr4xFIoMy_ijOe_l9XW0"
 
 
 module.exports = {
